ba592fa0c1f3817a713a3d424d3968309a4387ed
Daniel Neel <dneel10@gmail.com>
2023-08-04 00:36:43 -0400

    add CRUD operations for Kittens and corresponding views

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,50 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.create(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id]) 
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:alert] = "You have deleted a kitten. How dare you!"
+    redirect_to root_path
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
